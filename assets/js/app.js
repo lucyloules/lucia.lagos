9f402381a7d54d864dc9e1ae67a46fb75844350a
Lucyloules <lucia.lagos@gmail.com>
2018-02-19 15:27:36 -0300

    maquetado sin paleta de colores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
+$(document).ready(function () {
+  $('.parallax').parallax(); // parallax
+  $(".button-collapse").sideNav(); // SideNav Initialization
+});
+/* // Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+Ps.initialize(sideNavScrollbar);
 
+
+$(function initial() {
+  $(".element").typed({
+    strings: ["Web Designer / Front-End Developer"],
+    typeSpeed: 70,
+    showCursor: false,
+  });
+}); 
+ */
